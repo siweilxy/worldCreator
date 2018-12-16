4e6827cfc9abc20dd58563a92bf93851c952f04c
siweilxy <siweilxy@163.com>
2018-12-16 16:34:38 +0800

    change create secondes

--- src/main/java/com/siwei/worldCreator/GenerateSources.java
+++ src/main/java/com/siwei/worldCreator/GenerateSources.java
@@ -16,155 +16,155 @@ public class GenerateSources extends Thread {
 
 	private void checkAndAddSources(Sources sources) {
 		if (sources.getS1() > 100) {
-			sources.setS1(sources.getS1() + 1);
+			sources.setS1(sources.getS1() + 100000);
 		}
 
 		if (sources.getS2() > 100) {
-			sources.setS2(sources.getS2() + 1);
+			sources.setS2(sources.getS2() + 100000);
 		}
 		if (sources.getS3() > 100) {
-			sources.setS3(sources.getS3() + 1);
+			sources.setS3(sources.getS3() + 100000);
 		}
 		if (sources.getS4() > 100) {
-			sources.setS4(sources.getS4() + 1);
+			sources.setS4(sources.getS4() + 100000);
 		}
 		if (sources.getS5() > 100) {
-			sources.setS5(sources.getS5() + 1);
+			sources.setS5(sources.getS5() + 100000);
 		}
 		if (sources.getS6() > 100) {
-			sources.setS6(sources.getS6() + 1);
+			sources.setS6(sources.getS6() + 100000);
 		}
 		if (sources.getS7() > 100) {
-			sources.setS7(sources.getS7() + 1);
+			sources.setS7(sources.getS7() + 100000);
 		}
 		if (sources.getS8() > 100) {
-			sources.setS8(sources.getS8() + 1);
+			sources.setS8(sources.getS8() + 100000);
 		}
 		if (sources.getS9() > 100) {
-			sources.setS9(sources.getS9() + 1);
+			sources.setS9(sources.getS9() + 100000);
 		}
 		if (sources.getS10() > 100) {
-			sources.setS10(sources.getS10() + 1);
+			sources.setS10(sources.getS10() + 100000);
 		}
 		if (sources.getS11() > 100) {
-			sources.setS11(sources.getS11() + 1);
+			sources.setS11(sources.getS11() + 100000);
 		}
 		if (sources.getS12() > 100) {
-			sources.setS12(sources.getS12() + 1);
+			sources.setS12(sources.getS12() + 100000);
 		}
 		if (sources.getS13() > 100) {
-			sources.setS13(sources.getS13() + 1);
+			sources.setS13(sources.getS13() + 100000);
 		}
 		if (sources.getS14() > 100) {
-			sources.setS14(sources.getS14() + 1);
+			sources.setS14(sources.getS14() + 100000);
 		}
 		if (sources.getS15() > 100) {
-			sources.setS15(sources.getS15() + 1);
+			sources.setS15(sources.getS15() + 100000);
 		}
 		if (sources.getS16() > 100) {
-			sources.setS16(sources.getS16() + 1);
+			sources.setS16(sources.getS16() + 100000);
 		}
 		if (sources.getS17() > 100) {
-			sources.setS17(sources.getS17() + 1);
+			sources.setS17(sources.getS17() + 100000);
 		}
 		if (sources.getS18() > 100) {
-			sources.setS18(sources.getS18() + 1);
+			sources.setS18(sources.getS18() + 100000);
 		}
 		if (sources.getS19() > 100) {
-			sources.setS19(sources.getS19() + 1);
+			sources.setS19(sources.getS19() + 100000);
 		}
 		if (sources.getS20() > 100) {
-			sources.setS20(sources.getS20() + 1);
+			sources.setS20(sources.getS20() + 100000);
 		}
 		if (sources.getS21() > 100) {
-			sources.setS21(sources.getS21() + 1);
+			sources.setS21(sources.getS21() + 100000);
 		}
 		if (sources.getS22() > 100) {
-			sources.setS22(sources.getS22() + 1);
+			sources.setS22(sources.getS22() + 100000);
 		}
 		if (sources.getS23() > 100) {
-			sources.setS23(sources.getS23() + 1);
+			sources.setS23(sources.getS23() + 100000);
 		}
 		if (sources.getS24() > 100) {
-			sources.setS24(sources.getS24() + 1);
+			sources.setS24(sources.getS24() + 100000);
 		}
 		if (sources.getS25() > 100) {
-			sources.setS25(sources.getS25() + 1);
+			sources.setS25(sources.getS25() + 100000);
 		}
 		if (sources.getS26() > 100) {
-			sources.setS26(sources.getS26() + 1);
+			sources.setS26(sources.getS26() + 100000);
 		}
 		if (sources.getS27() > 100) {
-			sources.setS27(sources.getS27() + 1);
+			sources.setS27(sources.getS27() + 100000);
 		}
 		if (sources.getS28() > 100) {
-			sources.setS28(sources.getS28() + 1);
+			sources.setS28(sources.getS28() + 100000);
 		}
 		if (sources.getS29() > 100) {
-			sources.setS29(sources.getS29() + 1);
+			sources.setS29(sources.getS29() + 100000);
 		}
 		if (sources.getS30() > 100) {
-			sources.setS30(sources.getS30() + 1);
+			sources.setS30(sources.getS30() + 100000);
 		}
 		if (sources.getS31() > 100) {
-			sources.setS31(sources.getS31() + 1);
+			sources.setS31(sources.getS31() + 100000);
 		}
 		if (sources.getS32() > 100) {
-			sources.setS32(sources.getS32() + 1);
+			sources.setS32(sources.getS32() + 100000);
 		}
 		if (sources.getS33() > 100) {
-			sources.setS33(sources.getS33() + 1);
+			sources.setS33(sources.getS33() + 100000);
 		}
 		if (sources.getS34() > 100) {
-			sources.setS34(sources.getS34() + 1);
+			sources.setS34(sources.getS34() + 100000);
 		}
 		if (sources.getS35() > 100) {
-			sources.setS35(sources.getS35() + 1);
+			sources.setS35(sources.getS35() + 100000);
 		}
 		if (sources.getS36() > 100) {
-			sources.setS36(sources.getS36() + 1);
+			sources.setS36(sources.getS36() + 100000);
 		}
 		if (sources.getS37() > 100) {
-			sources.setS37(sources.getS37() + 1);
+			sources.setS37(sources.getS37() + 100000);
 		}
 		if (sources.getS38() > 100) {
-			sources.setS38(sources.getS38() + 1);
+			sources.setS38(sources.getS38() + 100000);
 		}
 		if (sources.getS39() > 100) {
-			sources.setS39(sources.getS39() + 1);
+			sources.setS39(sources.getS39() + 100000);
 		}
 		if (sources.getS40() > 100) {
-			sources.setS40(sources.getS40() + 1);
+			sources.setS40(sources.getS40() + 100000);
 		}
 		if (sources.getS41() > 100) {
-			sources.setS41(sources.getS41() + 1);
+			sources.setS41(sources.getS41() + 100000);
 		}
 		if (sources.getS42() > 100) {
-			sources.setS42(sources.getS42() + 1);
+			sources.setS42(sources.getS42() + 100000);
 		}
 		if (sources.getS43() > 100) {
-			sources.setS43(sources.getS43() + 1);
+			sources.setS43(sources.getS43() + 100000);
 		}
 		if (sources.getS44() > 100) {
-			sources.setS44(sources.getS44() + 1);
+			sources.setS44(sources.getS44() + 100000);
 		}
 		if (sources.getS45() > 100) {
-			sources.setS45(sources.getS45() + 1);
+			sources.setS45(sources.getS45() + 100000);
 		}
 		if (sources.getS46() > 100) {
-			sources.setS46(sources.getS46() + 1);
+			sources.setS46(sources.getS46() + 100000);
 		}
 		if (sources.getS47() > 100) {
-			sources.setS47(sources.getS47() + 1);
+			sources.setS47(sources.getS47() + 100000);
 		}
 		if (sources.getS48() > 100) {
-			sources.setS48(sources.getS48() + 1);
+			sources.setS48(sources.getS48() + 100000);
 		}
 		if (sources.getS49() > 100) {
-			sources.setS49(sources.getS49() + 1);
+			sources.setS49(sources.getS49() + 100000);
 		}
 		if (sources.getS50() > 100) {
-			sources.setS50(sources.getS50() + 1);
+			sources.setS50(sources.getS50() + 100000);
 		}
 	}
 
@@ -185,7 +185,7 @@ public class GenerateSources extends Thread {
 		this.setName("GenerateSources");
 		while (true) {
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(100000000);
 				updateSources();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
